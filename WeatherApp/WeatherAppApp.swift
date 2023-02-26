043e920afcda1454936d24bc0a8fc3dca5678967
Artyom Ivashchenko <artyom@Artyoms-MacBook-Pro.local>
2023-02-26 15:28:03 +0500

    Initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Artyom Ivashchenko on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
